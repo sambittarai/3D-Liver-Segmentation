8a76dbfddcd9634ef6505bae3706592d95e1c2f6
Sambit Tarai <sambitarai17@gmail.com>
2019-12-11 18:29:48 +0530

    Added comments

--- UNET3D/patch_extract.py
+++ UNET3D/patch_extract.py
@@ -9,29 +9,53 @@ from utils import adjust_center_for_boundaries, extract_patch, uniform_sample
 from utils import resample
 
 def sample_patches(lid, imgpath, lblpath):
+    '''
+    Extract Patches
+
+    Parameters:
+        imgpath - path of the image
+
+        lblpath - path of the corressponding segmented image
+
+        lid - 
+
+    '''
+
+    #Image
+
+    #Read the image and return an object (generator comprehension), Here 'image' is a sitk's object
     image = sitk.ReadImage(imgpath)
     image = resample(image, (1.0, 1.0, 1.0), interpolator = sitk.sitkLinear)
+    #get the pixel values from the image
     img_arr = sitk.GetArrayFromImage(image)
+    #Zooms the image according to the zoom
     img_arr = snd.zoom(img_arr, zoom = (0.5, 0.5, 0.5), order = 1)
-    img_arr = np.float32(np.clip(img_arr, -100, 400))
+    #pixel values are clipped b/w (-100, 400) and converted to float32
+    img_arr = np.float32(np.clip(img_arr, -100, 400))   #Why clipping b/w (-100,400)?
+    #Some kind of data preprocessing
     img_arr = np.uint8(255*(img_arr + 100)/(500))
+    #Pad the image
     img_arr = np.pad(img_arr, ((100,100),(100,100),(100,100)), mode = 'constant')
-        
+
+    #Label    
     label = sitk.ReadImage(lblpath)
     label = resample(label, (1.0, 1.0, 1.0), interpolator = sitk.sitkNearestNeighbor)
     lbl_arr = sitk.GetArrayFromImage(label)
     lbl_arr[lbl_arr == 2] = 1
     lbl_arr = np.uint8(snd.zoom(lbl_arr, zoom = (0.5, 0.5, 0.5), order = 0))
+    #Copies the content of 'lbl_arr' and adds '1' to it
     lbl_arr_cp = lbl_arr.copy() + 1
     lbl_arr = np.pad(lbl_arr, ((100,100),(100,100),(100,100)), mode = 'constant')
     lbl_arr_cp = np.pad(lbl_arr_cp, ((100,100),(100,100),(100,100)), mode = 'constant')
     lbl_arr_cp -= 1
     
+    #?
     class1_locs = uniform_sample(lbl_arr_cp == 0, 50)
     class2_locs = uniform_sample(lbl_arr_cp == 1, 50)
 #     print(' class 1, class 2 :', len(class1_locs), len(class2_locs))
     locs = class1_locs[:5] + class2_locs[:45]
     random.shuffle(locs)
+    #?
     
     patch_size, lbl_size = [116, 132, 132], [28, 44, 44]
     liver_pixel_count = {}
@@ -58,23 +82,20 @@ for lid in litsids:
     lblpath = os.path.join(data_root, 'segmentation-'+str(lid)+'.nii')
     img_lbl_pairs.append([lid, imgpath, lblpath])
 
-bar = progressbar.ProgressBar(len(img_lbl_pairs)).start()
+# bar = progressbar.ProgressBar(len(img_lbl_pairs)).start()
 # patch_save_root = './data'
 for step, [lid, imgpath, lblpath] in enumerate(img_lbl_pairs):
     sample_patches(lid, imgpath, lblpath)
-    bar.update(step+1)
+#    bar.update(step+1)
 
 
 """
 code for parallelization
 import multiprocessing as mp
-
-
 litsids = [102, 103, 104, 105, 107, 108]
 img_lbl_pairs = []
 data_root = '/mnt/data/LiverCT/Parenchyma/LITS'
 for lid in listids:
-	img_lbl_pairs.append()
-
+    img_lbl_pairs.append()
 pool = mp.Pool(3)
 """
